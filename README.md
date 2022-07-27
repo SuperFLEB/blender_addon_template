ebd91f2621e48f8b6c8aba0854b64acc5069f705
FLEB <voterudy@voterudy.org>
2022-07-26 20:42:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# A Blender Addon
+
+This is a Blender addon that allows you to do more in Blender than you could before.
+This Readme still nees to be written, as this is the dummy copy from the template.
+Don't that just lorem your ipsum?
+
+## To install
+
+Either install the ZIP file from the release, or move it to your Blender addons directory.
+
+## To use
+
+Do what comes naturally, I guess.
---
+++ src/preferences_panel.py
@@ -0,0 +1,16 @@
+import bpy
+
+package_name = __package__
+print("Preferences panel", __package__)
+
+class ThePreferencesPanel(bpy.types.AddonPreferences):
+    bl_idname = package_name
+    some_property: bpy.props.BoolProperty(
+        name="Turn on checkbox in preferences panel",
+        description="If this checkbox is checked, then it is checked",
+        default=False
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, 'some_property')
