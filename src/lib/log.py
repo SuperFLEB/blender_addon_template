16b3f3130aa0cfdd3c0dec87ac1b6f894f37b7f4
FLEB <voterudy@voterudy.org>
2026-01-15 13:10:17 -0500

    WIP on central log formatter

--- src/lib/log.py
+++ src/lib/log.py
@@ -1,4 +1,8 @@
+import sys
+import typing
 from enum import IntEnum
+from .ESQ import ESQ, ESQStyle
+
 
 class LogLevel(IntEnum):
     DEBUG = 0
@@ -7,10 +11,40 @@ class LogLevel(IntEnum):
     ERROR = 3
     CRITICAL = 4
 
+
 class ConsoleLogger:
-    def __init__(self, name: str, default_level: LogLevel = LogLevel.INFO):
+    style: ESQStyle | None
+    default_level: LogLevel
+    stdout_level: LogLevel
+    stderr_level: LogLevel
+    stdout: typing.TextIO | None
+    stderr: typing.TextIO | None
 
+    def __init__(self, name: str, style: ESQStyle | None, default_level: LogLevel = LogLevel.INFO,
+                 stdout: typing.TextIO = sys.stdout, stderr: typing.TextIO = sys.stderr,
+                 stdout_level: LogLevel = LogLevel.INFO, stderr_level: LogLevel = LogLevel.ERROR):
+        self.name = name
+        self.style = ESQ.default if style is None else style
+        self.default_level = default_level
 
-    def log(self, level: LogLevel, message: str) -> None:
+    def log(self, level: LogLevel = LogLevel.INFO, *messages) -> None:
+        if level < self.stdout_level and level < self.stderr_level: return
+        destination = self.stdout if level <= self.stderr_level else self.stderr
+        level_tag = "" if level is LogLevel.INFO else f":{LogLevel.name}"
+        log_line = "".join([str(m) for m in messages])
+        print(self.style(f"[{self.name}{level_tag}] {log_line}"), file=destination)
 
-        print(f"[{level.name}] {message}")
+    def debug(self, *messages) -> None:
+        self.log(LogLevel.DEBUG, *messages)
+
+    def info(self, *messages) -> None:
+        self.log(LogLevel.INFO, *messages)
+
+    def warning(self, *messages) -> None:
+        self.log(LogLevel.WARNING, *messages)
+
+    def error(self, *messages) -> None:
+        self.log(LogLevel.ERROR, *messages)
+
+    def critical(self, *messages) -> None:
+        self.log(LogLevel.CRITICAL, *messages)
